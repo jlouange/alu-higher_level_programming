b066e3a5be5943284e43c393bec87d25a2ad72bc
jlouange <j.mushumba@alustudent.com>
2026-06-24 12:37:10 -0700

    Question 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) in range(97, 122):
-        print("{}".format(char(ord(str)-32)))
-    else:
-        print("{}".format(str))
+    new_str = ""
+    for char in str:
+        ascii_code = ord(char)
+        if ascii_code in range(97, 123):
+            char = chr(ascii_code - 32)
+        new_str += char
+
+    print("{}".format(new_str))
