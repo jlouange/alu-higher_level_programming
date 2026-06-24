6c0d742a5455dc49cedd31e27376743b4ea43525
jlouange <j.mushumba@alustudent.com>
2026-06-24 05:52:46 -0700

    Question 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 import random
-try:
+
     
-    number = random.randint(-10000, 10000)
-    number_str = str(number)
-    last_digit = int(number_str[-1])
-    if number < 0:
-        last_digit = -last_digit
-    if last_digit > 5:
-        comparison = "and is greater than 5"
-    elif last_digit == 0:
-        comparison = "and is 0"
-    elif last_digit < 6 and last_digit != 0:
-        comparison = "and is less than 6 and not 0"
-    print("Last digit of {} is {} {}".format(number,last_digit,comparison))
-except ValueError:
-    print("Please enter a valid number!")
+number = random.randint(-10000, 10000)
+number_str = str(number)
+last_digit = int(number_str[-1])
+if number < 0:
+  last_digit = -last_digit
+if last_digit > 5:
+  comparison = "and is greater than 5"
+elif last_digit == 0:
+  comparison = "and is 0"
+elif last_digit < 6 and last_digit != 0:
+  comparison = "and is less than 6 and not 0"
+print("Last digit of {} is {} {}".format(number ,last_digit, comparison))
+
+
