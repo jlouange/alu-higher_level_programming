fb734712818054d2fb52fdfe6c0ad35752ea042d
jlouange <j.mushumba@alustudent.com>
2026-06-25 09:16:51 -0700

    Question 15: Additional

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+
+    new_str = ""
+    for i in range(len(str)):
+        if i != n:
+            new_str += str[i]
+        else:
+            continue
+    print(new_str)
