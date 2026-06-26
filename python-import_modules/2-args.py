5b1cc47659afffce87a29ecb2aab856a6b5c9c2a
jlouange <j.mushumba@alustudent.com>
2026-06-26 11:20:09 -0700

    Question 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     num_arg = len(argv) - 1
-    while True:
-        if num_arg == 1:
-            print("1 argument: ")
-            break
-        elif num_arg == 0:
-            print("0 arguments.")
-            break
-        else:
-            print("{} arguments:".format(num_arg))
-            for i in range(1, num_arg + 1):
-                print("{}: {}".format(i, argv[i]))
-                break
+    if num_arg == 1:
+        print("1 argument: ")
+    elif num_arg == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(num_arg))
+        for i in range(1, num_arg + 1):
+            print("{}: {}".format(i, argv[i]))
