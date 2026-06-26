8914fa0575aa581cd2fc0f95d937c09271f7ef2c
jlouange <j.mushumba@alustudent.com>
2026-06-26 11:33:20 -0700

    Question 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     num_arg = len(argv) - 1
     if num_arg == 1:
-        print("1 argument: ")
+        print("1 argument:")
     elif num_arg == 0:
         print("0 arguments.")
     else:
