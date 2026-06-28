af6f3f05b574904ff4c332be86f155495437c978
jlouange <j.mushumba@alustudent.com>
2026-06-28 16:00:29 -0700

    Question 6: Advanced

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator not in ("+", "-", "*", "/"):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if operator == "+":
+        print(add(a, b))
+    elif operator == "-":
+        print(sub(a, b))
+    elif operator == "*":
+        print(mul(a, b))
+    elif operator == "/":
+        if b == 0:
+            print("Can't divide by zero!")
+            sys.exit(1)
+        print(div(a, b))
