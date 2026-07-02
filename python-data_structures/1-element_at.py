64b4c60aa42d56889b1ded2113d783a09657deb4
jlouange <j.mushumba@alustudent.com>
2026-07-02 00:40:05 -0700

    Question 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,5 +5,4 @@ def element_at(my_list, idx):
     elif idx > len(my_list) - 1:
         return None
     if idx > 0 and idx <= len(my_list) - 1:
-        element = my_list[idx]
-        print("Element at index {} is {}".format(idx, element))
+        return my_list[idx]
