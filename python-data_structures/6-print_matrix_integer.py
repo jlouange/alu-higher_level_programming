861953d377faebe58182b0d06ddc370283d2f6cb
jlouange <j.mushumba@alustudent.com>
2026-07-04 07:42:55 -0700

    Question 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        matrix_integer = []
-        for j in i:
-            j = str(j)
-            matrix_integer.append(j)
-        joined = " ".join(matrix_integer)
-        print(joined)
-
+        print(*i)
+    
 matrix = [
             [1, 2, 3],
             [4, 5, 6],
