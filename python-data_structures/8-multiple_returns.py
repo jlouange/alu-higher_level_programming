f2f7269b2fd23bc19e246bd4811a6dce1819879c
jlouange <j.mushumba@alustudent.com>
2026-07-04 17:15:22 -0700

    Question 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,5 +4,6 @@ def multiple_returns(sentence):
         length_sentence = len(sentence)
         first_character = sentence[0]
     else:
+        length_sentence = len(sentence)
         first_character = "None"
     return(length_sentence, first_character)
