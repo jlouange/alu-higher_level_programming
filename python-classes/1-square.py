bc2dc05a1827a905f009055629544ec584306510
jlouange <j.mushumba@alustudent.com>
2026-07-19 03:36:47 +0200

    Question 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""okay """
+
+
+class Square:
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square's sides."""
+        self.size = size
