423e6522717cf2821ef4671c7554ef5c98a79c54
jlouange <j.mushumba@alustudent.com>
2026-07-19 04:26:29 +0200

    Question 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""okay """
+
+
+class Square:
+    """Class documentation"""
+    def __init__(self, size=0):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square's sides."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
