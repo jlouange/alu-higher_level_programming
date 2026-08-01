78830f18a8db08fd4ce09591dba73dbc3b6bd416
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:34:27 +0200

    Create table with unique ID constraint

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- Create table unique_id
+-- Create the unique_id table 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
 );
