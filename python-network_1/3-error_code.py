a9cc83f9626aadb6d2f87931eb19828556e25707
jlouange <j.mushumba@alustudent.com>
2026-08-01 09:37:09 +0200

    Handle HTTP errors using urllib

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Handles HTTP errors while fetching a URL using urllib."""
+
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code:", error.code)
