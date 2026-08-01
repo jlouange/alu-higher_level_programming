cbd472dce03fed2338d3beb901766128c275a3f2
jlouange <j.mushumba@alustudent.com>
2026-08-01 10:16:08 +0200

    Corrected

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Fetches the status from a remote server using requests."""
-
+"""
+documenting strings
+"""
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
