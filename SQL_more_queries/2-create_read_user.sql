40c2940727339d9e53b60cf0ef6f42495756218e
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:34:27 +0200

    Create database and read-only user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,12 @@
--- Create database hbtn_0d_2 and read-only user
+-- Creates hbtn_0d_2 database and user_0d_2 with SELECT-only access
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+EOF 
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
-
-GRANT SELECT
-ON hbtn_0d_2.*
-TO 'user_0d_2'@'localhost';
+cat << EOF > 3-force_name.sql
+-- Create force_name table 
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
