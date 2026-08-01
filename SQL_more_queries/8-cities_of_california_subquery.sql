09fdcf1d52db8c396785a5ef921fea114da59529
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:34:27 +0200

    List California cities using subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,4 @@
--- List all cities of California using a subquery
-SELECT
-    id,
-    name
-FROM cities
-WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
+-- Lists all the cities of California 
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY id ASC;
