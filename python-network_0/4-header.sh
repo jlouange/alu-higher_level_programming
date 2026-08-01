33f4665da0cea6a3468069ab33f674f91435f701
jlouange <j.mushumba@alustudent.com>
2026-08-01 07:42:51 +0200

    Corrected

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header
-curl -s -H "X-HolbertonSchool-User-Id: 98" ""
+# Sends a GET request with a custom header and displays the body of the response
+curl -sH "X-School-User-Id: 98" ""
