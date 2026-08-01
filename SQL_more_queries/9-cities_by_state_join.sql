f943ec9140cd9be0174e666d9758d014a7263f93
jlouange <j.mushumba@alustudent.com>
2026-08-01 04:46:42 +0200

    List cities with their states using JOIN

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- List all cities with their corresponding state
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
