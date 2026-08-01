eaafdf2a74219066c7a8a781065273214e0a37ad
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:47:23 +0200

    Refactor SQL query for cities by state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,13 +1,10 @@
--- Lists all cities with their state name
-SELECT cities.id, cities.name, states.name
+SELECT
+    cities.id,
+    cities.name,
+    states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
-EOF 
 
-cat << EOF > 10-genre_id_by_show.sql
--- Lists all shows with at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
