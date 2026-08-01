6d22a5b440e46ae73d4bb4c0f921cca6c0f2cefd
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:34:27 +0200

    Create states table with primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
--- Create the database hbtn_0d_usa and the states table
+-- Create database hbtn_0d_usq and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
 );
