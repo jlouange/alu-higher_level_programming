8a38e2f9109b7aeb3eb266e307ce83d52b97010f
jlouange <j.mushumba@alustudent.com>
2026-08-01 09:41:09 +0200

    Handle HTTP errors using requests

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Handles HTTP errors using requests."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
