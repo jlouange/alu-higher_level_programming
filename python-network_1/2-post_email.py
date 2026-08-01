86a68e7c282e83881addc148e74a7f649fe804bf
jlouange <j.mushumba@alustudent.com>
2026-08-01 09:36:09 +0200

    Send POST email request using urllib

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends an email using POST request with urllib."""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email})
+    data = data.encode("utf-8")
+
+    request = urllib.request.Request(url, data=data)
+
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
