732866faa811a86778c38fdb3c53c618a603951c
jlouange <j.mushumba@alustudent.com>
2026-08-01 06:34:27 +0200

    List cities with their states using JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,13 @@
--- List all cities with their corresponding state
-SELECT
-    cities.id,
-    cities.name,
-    states.name
+-- Lists all cities with their state name
+SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states
-ON cities.state_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
+EOF 
+
+cat << EOF > 10-genre_id_by_show.sql
+-- Lists all shows with at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
